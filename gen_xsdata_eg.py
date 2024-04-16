aa038819882feeee3eae313ff27df8ab100a8a8b
Pete Houghton <pete@investigatingsoftware.co.uk>
2024-04-16 19:40:38 +0000

    most of the structure using xsdata converted

--- gen_xsdata_eg.py
+++ gen_xsdata_eg.py
@@ -11,24 +11,23 @@ from pacs_008_001_08 import *
 import uuid
 
 
-# Create the root document element
-doc = Document()
 
-the_sttlinf = SettlementInstruction7()
-the_sttlinf.set_SttlmMtd("CLRG")
-the_sttlinf.set_ClrSys(ClearingSystemIdentification2Choice(Cd="STG"))
+
+clr_sys = ClearingSystemIdentification2Choice(cd="STG")
+the_sttlinf = SettlementInstruction7(sttlm_mtd="CLRG",
+                                     clr_sys=clr_sys)
 
 # Create the Group Header
-grp_header = GroupHeader93(MsgId="MIDTheMessageId",
-                           NbOfTxs=1,
-                           CreDtTm="2019-01-01T00:00:00",
-                           SttlmInf=the_sttlinf)
+grp_header = GroupHeader93(msg_id="MIDTheMessageId",
+                           nb_of_txs=1,
+                           cre_dt_tm="2019-01-01T00:00:00",
+                           sttlm_inf=the_sttlinf)
 
 
-pmt_id = PaymentIdentification7(InstrId="IXWEDRFTGHJK5",
-                                EndToEndId="E2EDRFGHJK7",
-                                TxId="TIDRFGHJ54678",
-                                UETR=uuid.uuid4())
+pmt_id = PaymentIdentification7(instr_id="IXWEDRFTGHJK5",
+                                end_to_end_id="E2EDRFGHJK7",
+                                tx_id="TIDRFGHJ54678",
+                                uetr=uuid.uuid4())
 
 
 zero_amt = ActiveCurrencyAndAmount("GBP", 0)
@@ -36,44 +35,40 @@ zero_amt = ActiveCurrencyAndAmount("GBP", 0)
 bic_1 = FinancialInstitutionIdentification18("BARCGB22")
 bic_2 = FinancialInstitutionIdentification18("VODAGB23")
 
-agt = BranchAndFinancialInstitutionIdentification6(FinInstnId=bic_1)
-chrgs_inf = Charges7(Amt=zero_amt, Agt=agt)
+agt = BranchAndFinancialInstitutionIdentification6(fin_instn_id=bic_1)
+chrgs_inf = Charges7(amt=zero_amt, agt=agt)
 
-ult_pstl = PostalAddress24(BldgNb="10",
-                           StrtNm="Cheapside",
-                           TwnNm="London",
-                           Ctry="GB")
-ult_debtr = PartyIdentification135(Nm="Mr Ulti Debtor",
-                                   PstlAdr=ult_pstl)
-
+ult_pstl = PostalAddress24(bldg_nm="10",
+                           strt_nm="Cheapside",
+                           twn_nm="London",
+                           ctry="GB")
+ult_debtr = PartyIdentification135(nm="Mr Ulti Debtor",
+                                   pstl_adr=ult_pstl)
+initd_pty = PartyIdentification135(nm="Ms Initd Party",
+                                   pstl_adr=ult_pstl)
 
 amt = ActiveCurrencyAndAmount("GBP", "555.01")
-cdtrtx = CreditTransferTransaction39(PmtId=pmt_id,
-                                     ChrgBr="SHAR",
-                                     IntrBkSttlmDt="2019-01-01",
-                                     IntrBkSttlmAmt=amt,
-                                     InstdAmt=amt,
-                                     ChrgsInf=[chrgs_inf],
-                                     InstdAgt=bic_1,
-                                     InstgAgt=bic_2,
-                                     UltmtDbtr=ult_debtr,
-                                     InitgPty= )
+cdtrtx = CreditTransferTransaction39(pmt_id=pmt_id,
+                                     chrg_br="SHAR",
+                                     intr_bk_sttlm_dt="2019-01-01",
+                                     intr_bk_sttlm_amt=amt,
+                                     instd_amt=amt,
+                                     chrgs_inf=[chrgs_inf],
+                                     instg_agt=bic_1,
+                                     instd_agt=bic_2,
+                                     ultmt_dbtr=ult_debtr,
+                                     initg_pty=initd_pty)
 
 
 # fit_to_fi_cust_cred_trans = FIToFICustomerCreditTransferV08()
 
-fit_to_fi_cust_cred_trans = FitoFicustomerCreditTransferV08()
+fit_to_fi_cust_cred_trans = FitoFicustomerCreditTransferV08(grp_hdr=grp_header,
+                                                             cdt_trf_tx_inf=[cdtrtx])
 
 
-fit_to_fi_cust_cred_trans.set_GrpHdr(grp_header)
-fit_to_fi_cust_cred_trans.set_CdtTrfTxInf([cdtrtx])
-
-
-doc.set_FIToFICstmrCdtTrf(fit_to_fi_cust_cred_trans)
-
+# Create the root document element
+doc = Document(fito_ficstmr_cdt_trf=fit_to_fi_cust_cred_trans)
 
 # Export to an XML file
 with open("payment_message.xml", "w") as xml_file:
     doc.export(xml_file, 0, name_='Document')
-
-
