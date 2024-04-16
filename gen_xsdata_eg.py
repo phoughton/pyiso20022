2286d66eba9085b3c6b3837cb869a4c238aa0ae0
Pete Houghton <pete@investigatingsoftware.co.uk>
2024-04-16 20:08:43 +0000

    more complete with renderering and appropriate namespacing

--- gen_xsdata_eg.py
+++ gen_xsdata_eg.py
@@ -2,18 +2,17 @@
 # from pacs_008_001_08 import GroupHeader93, SettlementInstruction7
 # from pacs_008_001_08 import ClearingSystemIdentification2Choice
 # from pacs_008_001_08 import CreditTransferTransaction39, PaymentIdentification7
-# from pacs_008_001_08 import ActiveCurrencyAndAmount, Charges7
 # from pacs_008_001_08 import BranchAndFinancialInstitutionIdentification6
 # from pacs_008_001_08 import FinancialInstitutionIdentification18
 # from pacs_008_001_08 import PartyIdentification135
 # from pacs_008_001_08 import PostalAddress24
 from pacs_008_001_08 import *
+from xsdata.formats.dataclass.serializers import XmlSerializer
+from xsdata.formats.dataclass.serializers.config import SerializerConfig
 import uuid
 
 
-
-
-clr_sys = ClearingSystemIdentification2Choice(cd="STG")
+clr_sys = ClearingSystemIdentification3Choice(cd="STG")
 the_sttlinf = SettlementInstruction7(sttlm_mtd="CLRG",
                                      clr_sys=clr_sys)
 
@@ -27,15 +26,19 @@ grp_header = GroupHeader93(msg_id="MIDTheMessageId",
 pmt_id = PaymentIdentification7(instr_id="IXWEDRFTGHJK5",
                                 end_to_end_id="E2EDRFGHJK7",
                                 tx_id="TIDRFGHJ54678",
-                                uetr=uuid.uuid4())
+                                uetr=str(uuid.uuid4()))
 
 
-zero_amt = ActiveCurrencyAndAmount("GBP", 0)
+zero_amt = ActiveOrHistoricCurrencyAndAmount("GBP", 0)
 
 bic_1 = FinancialInstitutionIdentification18("BARCGB22")
 bic_2 = FinancialInstitutionIdentification18("VODAGB23")
 
 agt = BranchAndFinancialInstitutionIdentification6(fin_instn_id=bic_1)
+
+instd_agt = BranchAndFinancialInstitutionIdentification6(fin_instn_id=bic_2)
+instg_agt = BranchAndFinancialInstitutionIdentification6(fin_instn_id=bic_1)
+
 chrgs_inf = Charges7(amt=zero_amt, agt=agt)
 
 ult_pstl = PostalAddress24(bldg_nm="10",
@@ -47,15 +50,16 @@ ult_debtr = PartyIdentification135(nm="Mr Ulti Debtor",
 initd_pty = PartyIdentification135(nm="Ms Initd Party",
                                    pstl_adr=ult_pstl)
 
-amt = ActiveCurrencyAndAmount("GBP", "555.01")
+instd_amt = ActiveOrHistoricCurrencyAndAmount(ccy="GBP", value=555.01)
+intra_bk_st_amt = ActiveCurrencyAndAmount(ccy="GBP", value=555.01)
 cdtrtx = CreditTransferTransaction39(pmt_id=pmt_id,
                                      chrg_br="SHAR",
                                      intr_bk_sttlm_dt="2019-01-01",
-                                     intr_bk_sttlm_amt=amt,
-                                     instd_amt=amt,
+                                     intr_bk_sttlm_amt=intra_bk_st_amt,
+                                     instd_amt=instd_amt,
                                      chrgs_inf=[chrgs_inf],
-                                     instg_agt=bic_1,
-                                     instd_agt=bic_2,
+                                     instg_agt=instg_agt,
+                                     instd_agt=instd_agt,
                                      ultmt_dbtr=ult_debtr,
                                      initg_pty=initd_pty)
 
@@ -70,5 +74,13 @@ fit_to_fi_cust_cred_trans = FitoFicustomerCreditTransferV08(grp_hdr=grp_header,
 doc = Document(fito_ficstmr_cdt_trf=fit_to_fi_cust_cred_trans)
 
 # Export to an XML file
-with open("payment_message.xml", "w") as xml_file:
-    doc.export(xml_file, 0, name_='Document')
+# with open("payment_message.xml", "w") as xml_file:
+#     doc.export(xml_file, 0, name_='Document')
+
+
+config = SerializerConfig(pretty_print=True)
+serializer = XmlSerializer(config=config)
+xml_string = serializer.render(doc, ns_map={None: "urn:iso:std:iso:20022:tech:xsd:pacs.008.001.08"})
+
+with open("my_model.xml", "w") as xml_file:
+    xml_file.write(xml_string)
