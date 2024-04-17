e1ae946cbcf5c81fc2e547cf89a01e3156e02ed1
Pete Houghton <pete@investigatingsoftware.co.uk>
2024-04-17 13:02:30 +0000

    Add head to ignore and repo genned libs

--- xsdata_gen.sh
+++ xsdata_gen.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 xsdata xsd/payments_clearing_and_settlement/pacs.008/pacs.008.001.08.xsd --package pacs.pacs_008_001_08
-xsdata xsd/business_application_header/head.001/head.001.001.02.xsd --package head.001.001.02.xsd
+xsdata xsd/business_application_header/head.001/head.001.001.02.xsd --package head.001.001.02
