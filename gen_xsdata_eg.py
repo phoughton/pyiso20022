2244ff444f063d926a170b3594d4a5fc37279b7c
Pete Houghton <pete@investigatingsoftware.co.uk>
2024-04-17 12:53:32 +0000

    Make message basic

--- gen_xsdata_eg.py
+++ gen_xsdata_eg.py
@@ -37,13 +37,23 @@ ult_pstl = PostalAddress24(bldg_nm="10",
                            strt_nm="Cheapside",
                            twn_nm="London",
                            ctry="GB")
+dbtr_pstl = PostalAddress24(bldg_nm="11",
+                            strt_nm="Farside",
+                            twn_nm="York",
+                            ctry="GB")
 ult_debtr = PartyIdentification135(nm="Mr Ulti Debtor",
                                    pstl_adr=ult_pstl)
-initd_pty = PartyIdentification135(nm="Ms Initd Party",
+initg_pty = PartyIdentification135(nm="Ms Initd Party",
                                    pstl_adr=ult_pstl)
-
+reg_debtr = PartyIdentification135(nm="Mrs Reg Debtor",
+                                   pstl_adr=dbtr_pstl)
+instr_for_nxt_agt = InstructionForNextAgent1(instr_inf="HOLD")
 instd_amt = ActiveOrHistoricCurrencyAndAmount(ccy="GBP", value=555.01)
 intra_bk_st_amt = ActiveCurrencyAndAmount(ccy="GBP", value=555.01)
+
+purp = Purpose2Choice(cd="PHON")
+rem_loc = RemittanceLocation7(rmt_id="BUSINESS DEBIT")
+rmt_inf = RemittanceInformation16(ustrd="INVOICE 123456")
 cdtrtx = CreditTransferTransaction39(pmt_id=pmt_id,
                                      chrg_br="SHAR",
                                      intr_bk_sttlm_dt="2019-01-01",
@@ -53,17 +63,24 @@ cdtrtx = CreditTransferTransaction39(pmt_id=pmt_id,
                                      instg_agt=instg_agt,
                                      instd_agt=instd_agt,
                                      ultmt_dbtr=ult_debtr,
-                                     initg_pty=initd_pty)
+                                     initg_pty=initg_pty,
+                                     dbtr=reg_debtr,
+                                     dbtr_agt=instg_agt,
+                                     cdtr_agt=instd_agt,
+                                     instr_for_nxt_agt=[instr_for_nxt_agt],
+                                     purp=purp,
+                                     rltd_rmt_inf=[rem_loc],
+                                     rmt_inf=rmt_inf)
 
 
 # fit_to_fi_cust_cred_trans = FIToFICustomerCreditTransferV08()
 
-fit_to_fi_cust_cred_trans = FitoFicustomerCreditTransferV08(grp_hdr=grp_header,
-                                                             cdt_trf_tx_inf=[cdtrtx])
+f2f_cust_cred_trans = FitoFicustomerCreditTransferV08(grp_hdr=grp_header,
+                                                      cdt_trf_tx_inf=[cdtrtx])
 
 
 # Create the root document element
-doc = Document(fito_ficstmr_cdt_trf=fit_to_fi_cust_cred_trans)
+doc = Document(fito_ficstmr_cdt_trf=f2f_cust_cred_trans)
 
 # Export to an XML file
 # with open("payment_message.xml", "w") as xml_file:
