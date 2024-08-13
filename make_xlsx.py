741ca2a8984acd0b73c26f992c07a0e1784ed406
Pete Houghton <pete@investigatingsoftware.co.uk>
2024-08-13 19:27:57 +0000

    Add working files

---
+++ make_xlsx.py
@@ -0,0 +1,4 @@
+import pyiso20022.tools as isotools
+
+isotools.camt053_to_excel("example_files/gs_camt/camt053_001_02.xml",
+                          "camt053_excel.xlsx")
--- tools/__init__.py
+++ tools/__init__.py
@@ -1,4 +1,3 @@
 from pyiso20022.tools.camt053_to_excel import camt053_to_excel
-from pyiso20022.tools.lookup import lookup
 
-__all__ = [camt053_to_excel, lookup]
+__all__ = [camt053_to_excel]
