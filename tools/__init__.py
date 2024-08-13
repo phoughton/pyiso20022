bee01a0d0afe854307a9b2786fc983fff673f5fc
Pete Houghton <pete@investigatingsoftware.co.uk>
2024-08-13 19:20:10 +0000

    camt53 to xlsx working - fixing path issues

---
+++ tools/__init__.py
@@ -0,0 +1,4 @@
+from pyiso20022.tools.camt053_to_excel import camt053_to_excel
+from pyiso20022.tools.lookup import lookup
+
+__all__ = [camt053_to_excel, lookup]
--- tools/camt053_to_excel.py
+++ tools/camt053_to_excel.py
@@ -1,27 +1,47 @@
 from lxml import etree
+import re
 import pandas as pd
+from pyiso20022.tools import lookup
 
 
-def modify_key(key, translate=True):
-    return key.split('}')[-1]
+def _iso20022_term_translator(mnemonic):
+    new_name = ""
+    list_of_mnems = re.split(r'(?<=[a-z])(?=[A-Z])', mnemonic)
+    for key in list_of_mnems:
+        new_name += lookup.mnemonics.get(key, key)
+        new_name += " "
+
+    return new_name.strip()
 
 
-def parse_element(element, parent_name=''):
+def _modify_key(key, translate=True):
+    clean_mnems = key.split('}')[-1]
+    if translate:
+        clean_mnems = _iso20022_term_translator(clean_mnems)
+    return clean_mnems
+
+
+def _parse_element(element, parent_name='', translate=True):
     data_dict = {}
     for child in element:
         child_name = f"{parent_name}_{child.tag}" if parent_name else child.tag
 
         if len(child):
-            data_dict.update(parse_element(child, child_name))
+            data_dict.update(_parse_element(child,
+                                            child_name,
+                                            translate=translate))
         else:
             data_dict[child_name] = child.text
 
-    modified_dict = {modify_key(k): v for k, v in data_dict.items()}
+    modified_dict = {_modify_key(k, translate=translate): v for k, v in data_dict.items()}
 
     return modified_dict
 
 
-def camt053_to_excel(xml_fname, excel_fname):
+def camt053_to_df(xml_fname, translate=True):
+    """
+    Create a pandas DataFrame from a camt053 xml file
+    """
     with open(xml_fname, "rb") as xml_file:
         xml_data = xml_file.read()
 
@@ -30,12 +50,15 @@ def camt053_to_excel(xml_fname, excel_fname):
     elements = root.xpath('//*[local-name()="Ntry"]')
 
     for record in elements:
-        record_data = parse_element(record)
+        record_data = _parse_element(record, translate=translate)
         data.append(record_data)
 
-    df = pd.DataFrame(data)
+    return pd.DataFrame(data)
 
+
+def camt053_to_excel(xml_fname, excel_fname, translate=True):
+    """
+    Create an excel file from a camt053 xml file
+    """
+    df = camt053_to_df(xml_fname, translate)
     df.to_excel(excel_fname, index=False)
-
-
-camt053_to_excel("example_files/gs_camt/camt053_001_02.xml", "camt053_001_02.xlsx")
